7db4a81c99fcc3a40c0c99b74137494937f4fab0
Evan Walker <walke208@mail.chapman.edu>
2017-09-11 00:55:37 +0000

    bash program that counts/echoes input value

---
+++ countup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "enter a positive integer" n
+if [ $n -lt 0 ]; then
+	echo argument must be a positive integer
+	exit 1
+fi
+
+count=1
+while [ $n -ge $count ];
+do
+ echo -n " $count"
+ let count+=1
+done
+echo
