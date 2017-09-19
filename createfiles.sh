e909267ee82751a8e9e1afed2f877e414fdb0b45
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-19 16:40:51 -0700

    Instructor comments

--- createfiles.sh
+++ createfiles.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mkdir tmpfiles
-cd tmpfiles
-for i in `seq 1 100`;
+cd tmpfiles                                 ### Since you entered a directory, it is polite to exit it again when done
+for i in `seq 1 100`;                       ### No semicolon needed, since you put a line break anyway
 do
-	echo Temporary file$i > file$i.tmp
+	echo Temporary file$i > file$i.tmp  ### Beware spaces. Put quotes around strings to prevent ambiguity when spaces are involved"
 done
+
+### cd - ### This will return to the original directory where the script was called
